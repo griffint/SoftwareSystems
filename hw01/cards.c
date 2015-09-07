c4e2478764c332476b77c389c551b11f7f40fb05
griffint <griffin.tschurwald@students.olin.edu>
2015-09-07 16:57:59 -0400

    adding cards.c

--- hw01/cards.c
+++ hw01/cards.c
@@ -2,35 +2,49 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
+/*
+* This function will determine the number value of the card based
+* on the user's string input. 
+* Returns: an integer
+*/
+int determine_card_number(char card_name[])
+{
+	int val;
+	while (card_name[0] != 'x')
+	{
+		switch(card_name[0])
+		{
+		case 'K':
+		case 'Q':
+		case 'J':
+			val = 10;
+			break;
+		case 'A':
+			val = 11;
+			break;
+		case 'X':
+			continue;
+		default:
+			val = atoi(card_name);
+			if ((val < 1) || (val > 10)) 
+			{
+				puts("I don't understand that value!");
+				continue;
+			}
+		}
+	}
+	
+	return val;
+}
+
 int main()
 {
 	char card_name[3];
 	int count = 0;
-	while (card_name[0] != 'x')
-	{
 		puts("Enter the card_name: ");
 		scanf("%2s", card_name);
-		int val = 0;
-		switch(card_name[0])
-		{
-			case 'K':
-			case 'Q':
-			case 'J':
-				val = 10;
-				break;
-			case 'A':
-				val = 11;
-				break;
-			case 'X':
-				continue;
-			default:
-				val = atoi(card_name);
-				if ((val < 1) || (val > 10)) 
-				{
-					puts("I don't understand that value!");
-					continue;
-				}
-		}
+		int val = determine_card_number(card_name);
 		if ((val>2) && (val < 7))
 		{
 			count++;
@@ -39,6 +53,5 @@ int main()
 			count--;
 		}
 		printf("Current Count: %i\n", count);
-	}
 	return 0;
 }
